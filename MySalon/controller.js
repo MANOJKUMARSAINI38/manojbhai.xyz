eabfc781a71b7ac6c98e38a51f43b6d487b8db5d
MANOJKUMARSAINI38 <63220464+MANOJKUMARSAINI38@users.noreply.github.com>
2025-09-30 07:42:28 +0530

    Update controller.js

--- MySalon/controller.js
+++ MySalon/controller.js
@@ -17,10 +17,12 @@ const pool = require("../config/db");
 exports.getSalons = async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM salon_profile");
-    res.json(result.rows || []);
+    res.status(200).json({success:true, message: result.rows.length > 0 
+        ? "successfound "
+        : "successbutnotfound",result:result.rows || []});
     console.log("resultsalonprofile", result.rows);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ success:false,error: err.message });
   }
 };
 
